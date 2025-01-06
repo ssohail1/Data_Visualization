87e30e9f713036f2c51cae2f14cfeeeeeb702362
Sidra <80551186+ssohail1@users.noreply.github.com>
2025-01-06 17:54:06 -0600

    Update Simulated_data_3Dgraph.R

--- scripts/Simulated_data_3Dgraph.R
+++ scripts/Simulated_data_3Dgraph.R
@@ -2,9 +2,11 @@
 library(vegan)
 library(plotly)
 library(dplyr)
+
 datatab <- data.frame(rnorm(100,1,1),rnorm(100,1,1),rnorm(100,1,1))
 datatab$type <- c(rep("M",14),rep("F",30),rep("MF",56))
 colnames(datatab) <- c("column1","column2","column3","type")
+
 # pco1,pco2,pco3 would be extracted from the eigenvalues after running pcoa on a distance matrix
 pco2 <- paste("PCo2",paste(paste("( ",threepercents[2],sep=''),'%',sep=''),sep = ' ', " )")
 pco1 <- paste("PCo1",paste(paste("( ",threepercents[1],sep=''),'%',sep=''),sep = ' ', " )")
